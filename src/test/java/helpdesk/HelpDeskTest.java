ba4143d66057c9ced8ba85c2bf954fac48440a1a
Dominik <saidamyzo@gmail.com>
2018-02-02 17:54:49 +0100

    Create test to check whether at time without service issue will not be handled, refactor helper methods

--- src/test/java/helpdesk/HelpDeskTest.java
+++ src/test/java/helpdesk/HelpDeskTest.java
@@ -1,6 +1,7 @@
 package helpdesk;
 
 import issue.Issue;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -9,15 +10,29 @@ import java.util.stream.IntStream;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class HelpDeskTest {
+    private Calendar calendar;
+    private HelpDesk helpDesk;
+
+    @BeforeMethod
+    private void setUp() {
+        calendar = mock(Calendar.class);
+        helpDesk = new HelpDesk(calendar);
+    }
+
     private static int[] allHours() {
         return IntStream.rangeClosed(0, 23).toArray();
     }
 
     private static int[] fridaysServiceHours() {
         return IntStream.rangeClosed(0, 17).toArray();
+    }
+
+    private static int[] fridaysNotServiceHours() {
+        return IntStream.rangeClosed(18, 23).toArray();
     }
 
     private static int[] daysWith24HourService() {
@@ -29,36 +44,56 @@ public class HelpDeskTest {
         };
     }
 
-    private List<Object[]> pairsDayHourWithWholeDayServiceDays() {
+    private static int[] weekend() {
+        return new int[]{
+                Calendar.SATURDAY,
+                Calendar.SUNDAY
+        };
+    }
+
+    private List<Object[]> cartesianProduct(int day, int[] hours) {
         List<Object[]> list = new ArrayList<>();
-        for (Object day: daysWith24HourService())
-            for (Object hour: allHours())
-                list.add(new Object[]{day, hour});
+        for (Object hour: hours)
+            list.add(new Object[]{day, hour});
         return list;
     }
 
-    private List<Object[]> fridaysProperHours() {
+    private List<Object[]> cartesianProduct(int[] days, int[] hours) {
         List<Object[]> list = new ArrayList<>();
-        for (Object hour: fridaysServiceHours())
-            list.add(new Object[]{Calendar.FRIDAY, hour});
+        for (int day: days)
+            list.addAll(cartesianProduct(day, hours));
         return list;
     }
 
     @DataProvider
     private Iterator<Object[]> serviceTime() {
         List<Object[]> list = new ArrayList<>();
-        list.addAll(pairsDayHourWithWholeDayServiceDays());
-        list.addAll(fridaysProperHours());
+        list.addAll(cartesianProduct(daysWith24HourService(), allHours()));
+        list.addAll(cartesianProduct(Calendar.FRIDAY, fridaysServiceHours()));
         return list.iterator();
     }
 
     @Test(dataProvider = "serviceTime")
     public void issuesShouldBeHandledIfComeInProperTime(int day, int hour) {
-        Calendar calendar = mock(Calendar.class);
         when(calendar.get(Calendar.DAY_OF_WEEK)).thenReturn(day);
         when(calendar.get(Calendar.HOUR_OF_DAY)).thenReturn(hour);
-        HelpDesk helpDesk = new HelpDesk(calendar);
 
         assertTrue(helpDesk.willHandleIssue(mock(Issue.class)));
+    }
+
+    @DataProvider
+    private Iterator<Object[]> timeWithoutService() {
+        List<Object[]> list = new ArrayList<>();
+        list.addAll(cartesianProduct(weekend(), allHours()));
+        list.addAll(cartesianProduct(Calendar.FRIDAY, fridaysNotServiceHours()));
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "timeWithoutService")
+    public void issuesShouldNotBeHandledIfComeInImproperTime(int day, int hour) {
+        when(calendar.get(Calendar.DAY_OF_WEEK)).thenReturn(day);
+        when(calendar.get(Calendar.HOUR_OF_DAY)).thenReturn(hour);
+
+        assertFalse(helpDesk.willHandleIssue(mock(Issue.class)));
     }
 }
